8072744e30f1bf593b2803dc0cd3e9404b62f556
Michael Jolley <mike@sparcapp.io>
2022-02-20 15:06:36 -0600

    Updating Discord intents

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -16,8 +16,7 @@ async function run(): Promise<void> {
       intents: [
         Intents.FLAGS.GUILDS, 
         Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_MESSAGES, 
-        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS
       ]
     })
